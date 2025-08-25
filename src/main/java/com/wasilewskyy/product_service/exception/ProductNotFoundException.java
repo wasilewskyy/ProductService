d28c49ba694a3ccbc07222ef2d002876cceb84c3
wasilewskyy <jakubwasilewski84@gmail.com>
2025-08-25 17:17:45 +0200

    xxx

--- src/main/java/com/wasilewskyy/product_service/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.wasilewskyy.product_service.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/wasilewskyy/product_service/model/entity/ProductType.java
+++
@@ -1,7 +0,0 @@
-package com.wasilewskyy.product_service.model.entity;
-
-public enum ProductType {
-    COMPUTER,
-    SMARTPHONE,
-    ELECTRONICS
-}
